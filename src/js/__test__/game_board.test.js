9d6e9f59d62a2bc5132063cc813f58528e065bc2
nazeh <ar.nazeh@gmail.com>
2019-10-25 15:47:32 +0300

    GameBoard checks the validity of placement positions thoroughly

--- src/js/__test__/game_board.test.js
+++ src/js/__test__/game_board.test.js
@@ -7,12 +7,12 @@ beforeEach(() => {
 });
 
 it('places a ship on the board', () => {
-  board.placeShip([88, 89, 90]);
-  expect(board.ships[0].positions).toEqual([88, 89, 90]);
+  board.placeShip([87, 88, 89]);
+  expect(board.ships[0].positions).toEqual([87, 88, 89]);
 });
 
 it('does not place a ship if position is occupied', () => {
-  board.placeShip([88, 89, 90]);
+  board.placeShip([87, 88, 89]);
   const res = board.placeShip([87, 88]);
 
   expect(res).toBeFalsy();
@@ -20,7 +20,7 @@ it('does not place a ship if position is occupied', () => {
 });
 
 it('does not place a ship if position too close to another ship', () => {
-  board.placeShip([88, 89, 90]);
+  board.placeShip([87, 88, 89]);
   const res = board.placeShip([98, 99]);
 
   expect(res).toBeFalsy();
@@ -33,4 +33,21 @@ it('considers the edge of the board', () => {
 
   expect(res).toBeTruthy();
   expect(board.ships.length).toBe(2);
+});
+
+it('doesnt add ship with non-sequential positions', () => {
+  expect(board.placeShip([2, 3, 5, 6])).toBeFalsy();
+  expect(board.placeShip([20, 30, 50, 60])).toBeFalsy();
+  expect(board.placeShip([2, 3, 4, 5])).toBeTruthy();
+  expect(board.placeShip([8, 18, 28, 38])).toBeTruthy();
+});
+
+it('doesnt add ship if positions contain out of bound position', () => {
+  expect(board.placeShip([99, 100])).toBeFalsy();
+  expect(board.placeShip([-1, 0])).toBeFalsy();
+});
+
+it('doesnt add ship across two rows', () => {
+  expect(board.placeShip([9, 10, 11])).toBeFalsy();
+  expect(board.placeShip([99, 100])).toBeFalsy();
 });
--- src/js/lib/game_board.js
+++ src/js/lib/game_board.js
@@ -25,11 +25,34 @@ const GameBoard = () => {
     (position) => invalidPositions.includes(position),
   );
 
-  // const notPlaceable = (positions) => false;
+  const notSequential = (positions) => {
+    const sorted = positions.sort();
+    const spread = Math.max(...sorted) - Math.min(...sorted);
+    return !(spread === positions.length - 1
+      || spread === (positions.length - 1) * 10);
+  };
+
+  const outOfBound = (positions) => positions.some((pos) => pos < 0 || pos > 99);
+
+  const crossRows = (positions) => {
+    const leftColumnElements = positions.filter(
+      (position) => position % 10 === 9,
+    );
+    if (leftColumnElements.length === 0) return false;
+    if (leftColumnElements[0] < Math.max(...positions)) return true;
+    return false;
+  };
+
+  const notPlaceable = (positions) => {
+    if (notSequential(positions)) return true;
+    if (outOfBound(positions)) return true;
+    if (crossRows(positions)) return true;
+    return false;
+  };
 
   const placeShip = (positions) => {
     if (isInvalid(positions)) return false;
-    // if (notPlaceable(positions)) return false;
+    if (notPlaceable(positions)) return false;
 
     ships.push(Ship(positions));
     storeInvalid(positions);
