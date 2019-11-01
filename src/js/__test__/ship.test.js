ea28c0d79c99e301e5a503dc9141ef6896ea24d5
nazeh <ar.nazeh@gmail.com>
2019-11-01 16:35:19 +0300

    update Ship factory

--- src/js/__test__/ship.test.js
+++ src/js/__test__/ship.test.js
@@ -1,5 +1,45 @@
 import Ship from '../lib/ship.js';
 
-it('s', () => {
-  expect(true).toBeTruthy();
+let cells;
+let ship;
+
+beforeEach(() => {
+  cells = [88, 89, 90];
+  ship = Ship(cells);
+});
+
+it('returns the cells it occupy', () => {
+  expect(ship.cells).toBe(cells);
+});
+
+it('returns its length', () => {
+  expect(ship.length).toBe(cells.length);
+});
+
+it('returns its hits', () => {
+  expect(ship.hits).toEqual([]);
+});
+
+it('add hits ', () => {
+  ship.hit(88);
+  expect(ship.hits).toEqual([88]);
+});
+
+it('doesnt add hits if already hit in this cell', () => {
+  ship.hit(88);
+  expect(ship.hit(88)).toBeFalsy();
+  expect(ship.hits).toEqual([88]);
+});
+
+it('doesnt add hits if out of its cells', () => {
+  expect(ship.hit(91)).toBeFalsy();
+  expect(ship.hits).toEqual([]);
+});
+
+it('checks if sunk', () => {
+  ship.hit(88);
+  expect(ship.isSunk()).toBeFalsy();
+  ship.hit(89);
+  ship.hit(90);
+  expect(ship.isSunk()).toBeTruthy();
 });
--- src/js/lib/ship.js
+++ src/js/lib/ship.js
@@ -1,5 +1,30 @@
-const Ship = () => {
+const Ship = (cells) => {
+  const { length } = cells;
+  const hits = [];
 
+  const hit = (cell) => {
+    if (!cells.includes(cell) || hits.includes(cell)) {
+      return false;
+    }
+    hits.push(cell);
+
+    return hits;
+  };
+  const isSunk = () => hits.length === length;
+
+  return {
+    get cells() {
+      return cells;
+    },
+    get length() {
+      return length;
+    },
+    get hits() {
+      return hits;
+    },
+    hit,
+    isSunk,
+  };
 };
 
 export default Ship;
